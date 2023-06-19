8421d6f00831c268d32da2a7b57e7552d89c28e5
nakaji <nakaji@un-t.com>
2023-06-19 14:50:47 +0900

    add

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  reactStrictMode: true
 }
+
 
 module.exports = nextConfig
